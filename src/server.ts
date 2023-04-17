f59f94c08db693181b7b8b202515d69ae62b77c1
MatheusGSantos <mths.gm@gmail.com>
2023-04-17 00:32:25 -0300

    feat: Adding AppError

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,25 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+
+import AppError from 'utils/AppError.js';
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
 });
 
 app.listen(8080, () => {
