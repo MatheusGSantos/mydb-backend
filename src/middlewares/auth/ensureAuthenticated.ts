78818f9128b9385d50ec2e71287faeba338c1aeb
MatheusGSantos <mths.gm@gmail.com>
2023-06-24 16:27:51 -0300

    .

---
+++ src/middlewares/auth/ensureAuthenticated.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from 'express';
+import { authConfig } from 'config/auth';
+import { verify } from 'jsonwebtoken';
+
+import AppError from 'utils/AppError';
+
+export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('Missing authentication token', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: user_id } = verify(token, authConfig.jwt.secret);
+
+    if (typeof user_id !== 'string') {
+      throw new AppError('Failed token verification', 401);
+    }
+
+    request.user = {
+      id: user_id,
+    };
+
+    return next();
+  } catch {
+    throw new AppError('token.invalid', 401);
+  }
+}
