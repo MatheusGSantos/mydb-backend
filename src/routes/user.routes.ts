03598422c6697063e453bf041c61e6aca0172078
MatheusGSantos <mths.gm@gmail.com>
2023-06-24 22:05:55 -0300

    feat: add authentication middleware

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { UserController } from 'controllers/UserController.js';
 import { validateUserFields } from 'middlewares/user/validateUserFields.js';
+import { ensureAuthenticated } from 'middlewares/auth/ensureAuthenticated';
 
 export const usersRoutes = Router();
 
 const usersController = new UserController();
 
 usersRoutes.get('/:userId?', usersController.show);
-usersRoutes.post('/', validateUserFields(), usersController.create);
-usersRoutes.patch('/:userId', validateUserFields({ attrsToOmit: ['password'] }), usersController.update);
-usersRoutes.delete('/:userId', usersController.delete);
+usersRoutes.post('/', ensureAuthenticated, validateUserFields(), usersController.create);
+usersRoutes.patch('/:userId', ensureAuthenticated, validateUserFields({ attrsToOmit: ['password'] }), usersController.update);
+usersRoutes.delete('/:userId', ensureAuthenticated, usersController.delete);
